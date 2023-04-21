8c97cf272688e6ddab9a28ea3f5805af0c2ec849
Yash Kaskar <yash.kaskar99@gmail.com>
2023-04-21 09:50:09 +0530

    button

--- booking-app/src/pages/PlacePage.jsx
+++ booking-app/src/pages/PlacePage.jsx
@@ -31,7 +31,7 @@ const PlacePage = () => {
                 ))}
                </div>
                <div>    
-                   <button className='ml-20 rounded-2xl flex gap-1 py-2 px-4 bg-zinc-500 text-white'>
+                   <button onClick={() => setshowAllPhotos()} className='ml-20 rounded-2xl flex gap-1 py-2 px-4 bg-zinc-500 text-white'>
                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                    </svg> 

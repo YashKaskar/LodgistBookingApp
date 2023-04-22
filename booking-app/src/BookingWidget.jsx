533372eefb3c2aa56db4adc6861edc1c7084e304
Yash Kaskar <yash.kaskar99@gmail.com>
2023-04-22 16:01:46 +0530

    booking

--- booking-app/src/BookingWidget.jsx
+++ booking-app/src/BookingWidget.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BookingWidget = ({place}) => {
+const BookingWidget = ({ place }) => {
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
   return (
     <div className='bg-white shadow p-4 rounded-xl text-black mt-9 md:mt-0'>
                       <div className='text-2xl font-bold text-center'> Price : ${place.price}/ per night</div>
                       <div className='my-4 p-4 rounded-2xl bg-gray-200'> 
                           <label>Check In : </label>
-                          <input className='bg-gray-200' type="date" />
+                          <input className='bg-gray-200' type="date" value={checkIn} onChange={ev => setCheckIn(ev.target.value)}/>
                       </div>
                       <div className='my-4 p-4 rounded-2xl bg-gray-200'> 
                           <label>Check Out : </label>
-                          <input className='bg-gray-200' type="date" />
+                          <input className='bg-gray-200' type="date" value={checkOut} onChange={ev => setCheckOut(ev.target.value)}/>
                       </div>
                       <div className='my-4 p-2 rounded-2xl bg-gray-200'> 
                           <label>Number of Guests : </label>
-                          <input className='bg-gray-200 border-stone-500' type="number" value={1} />
+                          <input type='number' value={numberOfGuests} onChange={ev => setNumberOfGuests(ev.target.value)} />
                       </div>
                       <div className='flex flex-col items-center'> 
                       <button className='w-44 mt-4  border rounded-xl py-2 px-2 text-lg font-medium text-lodgist1 bg-sky-400 '> Book The Place </button>

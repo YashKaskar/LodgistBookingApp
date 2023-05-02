d7492af0541cca0fd09709de1f519c18217c4e88
Yash Kaskar <yash.kaskar99@gmail.com>
2023-05-02 16:09:25 +0530

    .env file

--- booking-app/src/App.jsx
+++ booking-app/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8080';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 
